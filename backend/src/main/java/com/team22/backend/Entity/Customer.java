5cae831966a447d0809388d0f945bc0c4ce59fb8
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2019-02-04 03:10:04 +0700

    ไก่อ่อน

---
+++ backend/src/main/java/com/team22/backend/Entity/Customer.java
@@ -0,0 +1,43 @@
+package com.team22.backend.Entity;
+import lombok.*;
+import javax.persistence.*;
+import java.time.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+@Data
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name="Customer")
+public class Customer {
+    @Id
+    @SequenceGenerator(name="customer_seq",sequenceName="customer_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="customer_seq")
+    @Column(name="Customer_ID",unique = true, nullable = false)
+
+    @NotNull
+    private Long cusId;
+
+    private String customerIDs;
+
+    private String customerPassword;
+
+    private String customerName;
+    private String customerPhone;
+    private String customerGender;
+    private LocalDate customerBirthday;
+    private String customerAddress;
+
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Province.class)
+    @JoinColumn(name = "Province_ID", insertable = true)
+    private Province province;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Career.class)
+    @JoinColumn(name = "Career_ID", insertable = true)
+    private Career career;
+
+}
---
+++ frontend/src/app/signup/signup.component.ts
@@ -0,0 +1,159 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import {DatePipe} from '@angular/common';
+import { SignupService } from '../service/signup.service';
+import { Router } from '@angular/router';
+import {MatDialog , MatDialogRef} from '@angular/material';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  disableSelect = new FormControl(false);
+  pipe = new DatePipe('en-US');
+  hide = true;
+  customerRegister: Array<any>;
+  cusId: Array<any>;
+  customerIDs: Array<any>;
+  customerPassword: Array<any>;
+  customerName: Array<any>;
+  customerPhone: Array<any>;
+  customerGender: Array<any>;
+  customerBirthday: Array<any>;
+  customerAddress: Array<any>;
+  career: Array<any>;
+  province: Array<any>;
+  customer = {
+    cusId : '',
+    customerIDs : '',
+    customerPassword: '',
+    customerName: '',
+    customerPhone: '',
+    customerGender: '',
+    customerBirthday: '',
+    customerAddress: '',
+    career: '',
+    province: ''
+  };
+
+  constructor(private signupService: SignupService, private httpClient: HttpClient, private router: Router , public dialog: MatDialog) { }
+
+  ngOnInit() {
+    this.signupService.getCareer().subscribe(data => {
+      this.career = data;
+      console.log(this.career);
+    });
+    this.signupService.getProvince().subscribe(data => {
+      this.province = data;
+      console.log(this.province);
+    });
+
+  }
+  SignUp() {
+    if (
+      this.customer.customerIDs === '' ||
+     this.customer.customerPassword === '' ||
+     this.customer.customerName === '' ||
+     this.customer.customerPhone === '' ||
+     this.customer.customerGender === '' ||
+     this.customer.customerBirthday === '' ||
+     this.customer.customerAddress === '' ||
+     this.customer.career === '' ||
+     this.customer.province === '' ) {
+     alert('ข้อมูลไม่ครบถ้วน');
+} else {
+  this.signupService.CheckCustomer(this.customer.customerIDs).subscribe(checkCustomerData => {
+    console.log( checkCustomerData );
+        if ( checkCustomerData != null ) {
+         localStorage.setItem('customerIDs', JSON.stringify(checkCustomerData));
+         const dialogRef = this.dialog.open(CheckCustomerNotUse, {
+          width: '500px'
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          console.log('Can Not Use');
+        });
+        } else {
+          this.httpClient.post('http://localhost:8080/customerSignup/' + this.customer.customerIDs + '/'
+  + this.customer.customerPassword + '/' + this.customer.customerName +
+      '/' + this.customer.customerPhone + '/' + this.customer.customerGender +
+      '/' + this.pipe.transform(this.customer.customerBirthday, 'dd:MM:yyyy') + '/' +
+      this.customer.customerAddress + '/' +
+      this.customer.career + '/' +
+      this.customer.province, this.customerRegister)
+  .subscribe(
+    dataRegister => {
+      window.location.reload();
+        window.location.href = '/home';
+        console.log('Post successful', dataRegister);
+        alert('สำเร็จ');
+        },
+    error => {
+        console.log('Error', error);
+      }
+      );
+      }
+   });
+  }
+        console.log( this.customer.customerIDs,
+        this.customer.customerPassword,
+        this.customer.customerName,
+        this.customer.customerPhone,
+        this.customer.customerGender,
+        this.customer.customerBirthday,
+        this.customer.customerAddress,
+        this.customer.career,
+        this.customer.province);
+ }
+  checkId(id) {
+    this.signupService.CheckCustomer(this.customer.customerIDs).subscribe(checkCustomer => {
+      console.log( checkCustomer );
+          if ( checkCustomer != null ) {
+           localStorage.setItem('customerIDs', JSON.stringify(checkCustomer));
+           const dialogRef = this.dialog.open(CheckCustomerNotUse, {
+            width: '500px'
+          });
+          dialogRef.afterClosed().subscribe(result => {
+            console.log('Can Not Use');
+          });
+          } else {
+             const dialogRef = this.dialog.open(CheckCustomerCanUse, {
+                            width: '500px'
+                          });
+                          dialogRef.afterClosed().subscribe(result => {
+                            console.log('Can Use');
+                          });
+          }
+   });
+    console.log(id);
+  }
+
+}
+
+@Component ({
+  selector: 'app-checkCustomerCanUse',
+  templateUrl: './checkCustomerCanUse.html',
+})
+// tslint:disable-next-line:component-class-suffix
+export class CheckCustomerCanUse {
+  constructor(
+    public dialogRef: MatDialogRef<CheckCustomerCanUse>
+    ) {}
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+@Component ({
+  selector: 'app-checkCustomerNotUse',
+  templateUrl: './checkCustomerNotUse.html',
+})
+// tslint:disable-next-line:component-class-suffix
+export class CheckCustomerNotUse {
+  constructor(
+    public dialogRef: MatDialogRef<CheckCustomerNotUse>
+    ) {}
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
