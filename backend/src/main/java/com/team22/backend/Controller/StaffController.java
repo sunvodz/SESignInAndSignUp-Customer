f050a49f81a664a4cac32814bd9d576849a13e88
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2019-02-09 23:23:23 +0700

    ก็แก้ดิครับ

--- backend/src/main/java/com/team22/backend/Controller/StaffController.java
+++ backend/src/main/java/com/team22/backend/Controller/StaffController.java
@@ -48,7 +48,10 @@ class StaffController {
     public Collection<Experience> experience() {
         return experienceRepository.findAll().stream().collect(Collectors.toList());
     }
-
+    @PostMapping("/staffCheck/{staffIds}")
+    public Staff staffCheck(@PathVariable String staffIds) {
+        return this.staffRepository.findByStaffIds(staffIds);
+    }
     @PostMapping("/staffs/{staffName}/{staffGender}/{educationId}/{staffPhone}/{staffJobtype}/{staffSalary}/{positionId}/{staffStatus}/{experienceId}")
     public Staff newStaff(
             @PathVariable String staffName,
